2c35186b476b61e727ea3cee9ffc6c292bfa85b4
qinyu <yuqin@faceunity.com>
2021-11-26 15:38:50 +0800

    补充dart部分遗漏的

--- fulive_flutter/lib/Tools/FUImagePixelRatio.dart
+++ fulive_flutter/lib/Tools/FUImagePixelRatio.dart
@@ -12,6 +12,8 @@ class FUImagePixelRatio {
       ///在设备像素比率为1.8的设备上，.../2.0x/my_icon.png 将被选择。对于2.7的设备像素比率，.../3.0x/my_icon.png将被选择。
       /// iphone 上面不是这个规则，以 逻辑点距和像素点之间关系来决定几倍图 例如iphone 屏幕像素比和点距比例例为 1.78, 但是用的2x图片来显示，所以需要区分平台来加载，这里iOS 统一用高分别率图片。不区分了
       imagePath = imagePath + '3.0x/';
+    } else if (Platform.isAndroid) {
+      imagePath = imagePath + '3.0x/';
     } else {
       imagePath = pathPre;
     }
